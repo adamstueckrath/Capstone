f78cbfb6ca70a109cd2043a06f939e839b0d1f8d
unknown <rexiao@PBB-C320R-09.iowa.uiowa.edu>
2014-04-27 21:23:10 -0500

    Added rankall

---
+++ rankall.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/MasterPage.master" AutoEventWireup="false" CodeFile="rankall.aspx.vb" Inherits="rankall" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <div>
+    
+        <br />
+        <asp:Panel ID="Panel1" runat="server">
+            Financial Statement to use:&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList3" runat="server">
+                <asp:ListItem>Quarterly</asp:ListItem>
+                <asp:ListItem>Annually</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            Sector or Industry:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="True">
+                <asp:ListItem Value="All">All Industry</asp:ListItem>
+                <asp:ListItem Value="Basic Materials">Basic Materials</asp:ListItem>
+                <asp:ListItem Value="Consumer Goods">Consumer Goods</asp:ListItem>
+                <asp:ListItem Value="Financial">Financial</asp:ListItem>
+                <asp:ListItem Value="Healthcare">Healthcare</asp:ListItem>
+                <asp:ListItem Value="Industrial Goods">Industrial Goods</asp:ListItem>
+                <asp:ListItem Value="Services">Services</asp:ListItem>
+                <asp:ListItem Value="Technology">Technology</asp:ListItem>
+                <asp:ListItem Value="Utilities">Utilities</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+                <asp:ListItem Value="AC">Accruals</asp:ListItem>
+                <asp:ListItem Value="AG">Assets Growth</asp:ListItem>
+                <asp:ListItem Value="AT">Assets Turnover</asp:ListItem>
+                <asp:ListItem Value="EY">Earnings Yield</asp:ListItem>
+                <asp:ListItem Value="GM">Gross Margin</asp:ListItem>
+                <asp:ListItem Value="MC">Market Cap</asp:ListItem>
+                <asp:ListItem Value="PE">P/E</asp:ListItem>
+                <asp:ListItem Value="RDR">R&D Expenditure Ratio</asp:ListItem>
+                <asp:ListItem Value="ROC">Return on Capital</asp:ListItem>
+                <asp:ListItem Value="SG">Sales Growth</asp:ListItem>
+            </asp:CheckBoxList>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Generate Rank" Width="129px" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" Text="Quick Rank" />
+            <br />
+        </asp:Panel>
+    
+    </div>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:cs_PVI %>" SelectCommand="SELECT [Company Name], [Ticker], [Sector], [Industry] FROM [PVI_Ranks]"></asp:SqlDataSource>
+        <asp:GridView ID="GridView1" runat="server" CssClass="table table-hover">
+        </asp:GridView>
+
+
+</asp:Content>
+
---
+++ rankall.aspx.vb
@@ -0,0 +1,96 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+
+Partial Class rankall
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("cs_PVI").ConnectionString
+        Dim selectClause As String = "SELECT [Company Name] as Company, [Ticker] as Ticker, [Sector] as Sector, [Industry] as Industry"
+
+        Using myConnection As New SqlConnection(connectionString)
+            myConnection.Open()
+
+
+            'Handle the ratio selected in checkbox
+            For Each item As ListItem In CheckBoxList1.Items
+                If item.Selected Then
+                    selectClause += ", " & "[" & item.Value & "], " & "[" & item.Value + " Rank" & "]"
+                ElseIf item.Selected = False Then
+                    selectClause = selectClause
+                End If
+            Next
+
+            Dim sqlstring As String = SqlDataSource1.SelectCommand
+
+            'Handle the sector selected in dropdownlist 
+            If DropDownList2.SelectedValue <> "All" Then
+                sqlstring = selectClause + " FROM [PVI_Ranks] WHERE [Sector] = '" & DropDownList2.SelectedValue & "'"
+            ElseIf DropDownList2.SelectedValue = "All" Then
+                sqlstring = selectClause + " FROM [PVI_Ranks]"
+            End If
+
+            'Display the sql result in girdview
+            Dim mycommand As New SqlCommand(sqlstring, myConnection)
+            Dim reader As SqlDataReader = mycommand.ExecuteReader()
+
+            GridView1.DataSource = reader
+            GridView1.DataBind()
+
+        End Using
+    End Sub
+
+    'When dropdownlist selecteditem changed, automatically update the gridview
+    Protected Sub DropDownList2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList2.SelectedIndexChanged
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("cs_PVI").ConnectionString
+        Dim selectClause As String = "SELECT [Company Name] as Company, [Ticker] as Ticker, [Sector] as Sector, [Industry] as Industry"
+
+        Using myConnection As New SqlConnection(connectionString)
+            myConnection.Open()
+
+
+            'Handle the ratio selected in checkbox
+            For Each item As ListItem In CheckBoxList1.Items
+                If item.Selected Then
+                    selectClause += ", " & "[" & item.Value & "], " & "[" & item.Value + " Rank" & "]"
+                ElseIf item.Selected = False Then
+                    selectClause = selectClause
+                End If
+            Next
+
+            Dim sqlstring As String = SqlDataSource1.SelectCommand
+
+            'Handle the sector selected in dropdownlist 
+            If DropDownList2.SelectedValue <> "All" Then
+                sqlstring = selectClause + " FROM [PVI_Ranks] WHERE [Sector] = '" & DropDownList2.SelectedValue & "'"
+            ElseIf DropDownList2.SelectedValue = "All" Then
+                sqlstring = selectClause + " FROM [PVI_Ranks]"
+            End If
+
+            'Display the sql result in girdview
+            Dim mycommand As New SqlCommand(sqlstring, myConnection)
+            Dim reader As SqlDataReader = mycommand.ExecuteReader()
+
+            GridView1.DataSource = reader
+            GridView1.DataBind()
+
+        End Using
+    End Sub
+
+    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("cs_PVI").ConnectionString
+        Dim selectClause As String = "SELECT * FROM [PVI_Ranks]"
+
+        Using myConnection As New SqlConnection(connectionString)
+            myConnection.Open()
+
+            Dim mycommand As New SqlCommand(selectClause, myConnection)
+            Dim reader As SqlDataReader = mycommand.ExecuteReader()
+
+            GridView1.DataSource = reader
+            GridView1.DataBind()
+
+        End Using
+    End Sub
+End Class
